dfe67749fbec2f1e797af493ab94fea979f29d48
aleapos <alexandris_tolis@yahoo.gr>
2022-11-21 20:28:59 +0200

    this is my first commit

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
-url = "https://en.wikipedia.org/wiki/List_of_highest-paid_film_actors"
-url_txt = requests.get(url).text
-s= BeautifulSoup(url_txt,'lxml')
+import pandas as pd
+import matplotlib.pyplot as plt
 
-my_table = s.find('table', class_ = 'wikitable sortable plainrowheaders')
-table_links = my_table.find_all('a',href= True)
-actors = []
-for links in table_links:
-    actors.append(links.get('title'))
+data = pd.read_csv('2016-2019_finance_liquor_sales.csv')
 
-print(actors)
+df1 = pd.DataFrame(data, columns=['zip_code', 'item_description', 'bottles_sold'])
+df1.groupby('zip_code').bottles_sold.max()
+idx = df1.groupby('zip_code').bottles_sold.transform(max) == df1['bottles_sold']
+print(df1[idx])
+
+df2 = pd.DataFrame(data, columns=['store_name','sale_dollars'])
+x= df2.groupby('store_name').sale_dollars.sum()
+xdf = pd.DataFrame(x)
+xdf['sales_perc'] =round((xdf['sale_dollars'] / xdf['sale_dollars'].sum()) * 100, 2)
+print(xdf)
+
+
+
+
